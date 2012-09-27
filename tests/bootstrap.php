ca4cbbbe9431682955bd899f33c6fb25cd79b16e
omissis <claudi0.beatric3@gmail.com>
2012-09-27 10:47:21 +0200

    added test folder and composer's include_path to test bootstrap include paths

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,8 +3,15 @@
 // Add src path is on include_path
 set_include_path(implode(PATH_SEPARATOR, array(
     realpath(dirname(__FILE__) . '/../src'),
+    realpath(dirname(__FILE__) . '/src'),
     get_include_path(),
 )));
+
+// set path to add the include_paths exposed by composer
+$composer_include_paths = require_once dirname(__DIR__) . '/vendor/composer/include_paths.php';
+foreach ($composer_include_paths as $path) {
+    set_include_path($path . PATH_SEPARATOR . get_include_path());
+}
 
 require_once 'Zend/Loader/Autoloader.php';
 $autoloader = Zend_Loader_Autoloader::getInstance();
